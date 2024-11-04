f51e0051cb49c66a7d5b158aba5ae7c15101fbcb
BorgePambo <BorgePambo>
2024-11-04 22:04:20 +0000

    segundo commit

---
+++ Ref Databricks azure.py
@@ -0,0 +1,26 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC #######Referencia Databricks
+# MAGIC
+# MAGIC  "o que é lakehouse:\n",
+# MAGIC   https://learn.microsoft.com/pt-br/azure/databricks/lakehouse/
+# MAGIC   
+# MAGIC   O que é arquitetura medallion do Lakehouse?\n",
+# MAGIC   https://learn.microsoft.com/pt-br/azure/databricks/lakehouse/medallion
+# MAGIC     
+# MAGIC   Pipelines de processamento de ingestão, ETL e fluxo com o Azure Databricks e o Delta Lake
+# MAGIC   https://learn.microsoft.com/pt-br/azure/architecture/solution-ideas/articles/ingest-etl-stream-with-adb
+# MAGIC     
+# MAGIC   Grupos de recursos do Azure\n",
+# MAGIC   https://learn.microsoft.com/pt-br/azure/azure-resource-manager/management/manage-resource-groups-portal
+# MAGIC
+# MAGIC   Introdução ao Azure Data Lake Storage Gen2\n
+# MAGIC   https://learn.microsoft.com/pt-br/azure/storage/blobs/data-lake-storage-introduction
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
